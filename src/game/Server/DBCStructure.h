f15ec70a3c9cf7a9e328ac8705dbe6f4aeed3f62
Mark <Mark@markstreeton.co.uk>
2026-07-13 21:16:06 +0100

    feat(four-align): align ItemArmorShield to 5.4.8 client names (Id, Id2, Value)

--- src/game/Server/DBCStructure.h
+++ src/game/Server/DBCStructure.h
@@ -1205,9 +1205,9 @@ struct ItemArmorQualityEntry
 
 struct ItemArmorShieldEntry
 {
-  uint32    Id;                                             // 0 item level
-  uint32    Id2;                                            // 1 item level
-  float     Value[7];                                       // 2-8 multiplier for item quality
+  uint32    ID;                                             // 0 item level
+  uint32    ItemLevel;                                            // 1 item level
+  float     Quality[7];                                       // 2-8 multiplier for item quality
 };
 
 struct ItemArmorTotalEntry
