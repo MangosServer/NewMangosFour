0d208d0fd6d6737bf5af05813afcf4f4c33e30bb
MadMax <madmax@getmangos.eu>
2026-07-24 22:25:50 +0100

    build: prefer generated revision headers

--- src/game/Server/tests/mop_core_banner_source_test.cmake
+++ src/game/Server/tests/mop_core_banner_source_test.cmake
@@ -1,4 +1,5 @@
 file(READ "${SOURCE_ROOT}/cmake/MangosParams.cmake" mangos_params)
+file(READ "${SOURCE_ROOT}/src/shared/CMakeLists.txt" shared_cmake)
 file(READ "${SOURCE_ROOT}/src/shared/Utilities/Util.cpp" util)
 
 string(REGEX MATCH "set\\(MANGOS_EXP[ \t]+\"MISTS\"\\)" expansion_define
@@ -13,4 +14,12 @@ string(REGEX MATCH
 if(NOT mop_banner_branch)
     message(FATAL_ERROR
         "return_iCoreNumber does not map the canonical MISTS build define to Four")
+endif()
+
+string(REGEX MATCH
+    "PUBLIC[\r\n\t ]+[$][{]CMAKE_CURRENT_BINARY_DIR[}][\r\n\t ]+[$][{]CMAKE_CURRENT_SOURCE_DIR[}]"
+    generated_headers_first "${shared_cmake}")
+if(NOT generated_headers_first)
+    message(FATAL_ERROR
+        "shared target does not prefer generated headers over stale source-tree copies")
 endif()
